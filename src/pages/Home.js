25e076befb215c6bca0bf6558321a99cef2c0656
MAYANK AGARWAL <63557923+mayankag30@users.noreply.github.com>
2021-05-14 00:27:48 +0530

    Footer, Team Section Updated

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import { Route } from "react-router-dom";
+// import HomeMarq from "../components/HomeMarq";
+
+const Home = () => {
+  //   return <Home />;
+  return <h1>HOME</h1>;
+};
+
+export default Home;
