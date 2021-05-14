6308d8d9f36de80bec0ca665f3ec8b9dae1aa389
MAYANK AGARWAL <63557923+mayankag30@users.noreply.github.com>
2021-05-14 20:07:43 +0530

    Delete src directory

--- src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import { Route } from "react-router-dom";
-// import HomeMarq from "../components/HomeMarq";
-
-const Home = () => {
-  //   return <Home />;
-  return <h1>HOME</h1>;
-};
-
-export default Home;
